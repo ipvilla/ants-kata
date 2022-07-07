172d40b2402f6bd0ed94ec6bac78a33e44e786ee
Isa Pérez <isperez@aidacanarias.com>
2022-07-07 19:00:06 +0200

    ♻ Refactor in production code and renamed solution

--- AntApp/AntApp.cs
+++ AntApp/AntApp.cs
@@ -2,18 +2,11 @@ namespace AntApp
 {
     public class AntApp
     {
-        private char[,] grid;
+        private readonly char[,] grid;
 
         public AntApp(int dimension)
         {
-            if (dimension == 5)
-            {
-                grid = new char[5, 5];
-            }
-            else
-            {
-                grid = new char[3, 3];
-            }
+            grid = new char[dimension, dimension];
         }
 
         public char[,] GetGrid()
