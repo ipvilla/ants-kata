69fb732c3cf5ce837c3c39b8d67f061f6a14fe36
Isa Pérez <isperez@aidacanarias.com>
2022-07-07 18:29:55 +0200

    Initial commit

---
+++ AntAppSpecs/AntAppShould.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace AntAppSpecs
+{
+    public class AntAppShould
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
